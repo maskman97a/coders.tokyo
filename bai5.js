c875ea38021e51f5d7edd5f9f156a98474a44815
maskman97a <44739831+maskman97a@users.noreply.github.com>
2019-06-30 10:02:18 +0700

    Create bai5.js

---
+++ bai5.js
@@ -0,0 +1,14 @@
+/**
+ * Sử dụng kiến thức đã học khai báo một vài object khác nhau mà bạn có thể tưởng tượng ra. Ví dụ:
+ *  var myCommunity = {
+ *    name: 'Coders.Tokyo',
+ *    numberOfMembers: 10000,
+ *    isAwesome: true
+ *  };
+ */
+var student = {
+    ten: 'tung',
+    mssv:'1524242',
+    tuoi:22,
+    gioitinh: 'nam'
+  }
