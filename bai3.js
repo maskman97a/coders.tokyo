dbb4fc02fc0592f7ca2c7647380bb89cfe3dd469
maskman97a <44739831+maskman97a@users.noreply.github.com>
2019-06-30 09:56:19 +0700

    Bai 3

---
+++ bai3.js
@@ -0,0 +1,13 @@
+// Bài tập
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+var x = 1;
+var y = 2;
+var tong;
+var hieu;
+var tich;
+var thuong;
+tong = x + y;
+hieu = x - y;
+tich = x * y;
+thuong = x / y;
